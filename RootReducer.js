ebae0a9bc02d31194535bf97a7c10afec5f65a0d
alok kumar chaurasia <96462539+alok7136@users.noreply.github.com>
2021-12-24 18:46:28 +0530

    Add files via upload
    there are reducer and rootreducer which carry all reducer data

---
+++ RootReducer.js
@@ -0,0 +1,6 @@
+import ChangeTheNumber from "./incdec";
+import { combineReducers } from "redux";
+const rootReducer = combineReducers({
+    ChangeTheNumber  // we can put all the reducer here and export root reducer
+})
+export default rootReducer;
