037de0dc273427cc2bf57e4d1bf937d3aa05265c
Alok Kumar Chaurasia <96462539+alok7136@users.noreply.github.com>
2022-02-15 01:46:39 +0530

    Update calci.js

--- calci.js
+++ calci.js
@@ -1,34 +1,86 @@
-var display = document.querySelector('#display');
-var buttons = document.querySelectorAll('button');
-var displayValue = '';
-for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        buttonValue = e.target.value;
-        if (buttonValue == '*') {
-            displayValue += buttonValue;
-            display.value = displayValue;
-        }
-        else if (buttonValue == 'C') {
-            displayValue = "";
-            display.value = displayValue;
-        }
-        else if (buttonValue == '=') {
-            display.value = eval(displayValue);
-        }
-        else {
-            displayValue += buttonValue;
-            display.value = displayValue;
-        }
-    })
+var selection = 'radian'
+
+function radian(){
+  var selectionvalue = document.getElementsByClassName("radian")
+  selectionvalue.style = "background: solid red"
+  var selectiondegree = document.getElementsByClassName("degree")
+  selectiondegree.style = "font: solid Yellow"
+  selection = "degree"
 }
 
-function sin()
-{
-    display.value =Math.sin(display.value)
+function degree(){
+  var selectionvalue = document.getElementsByClassName("radian")
+  selectionvalue.style = "font:solid yellow"
+  var selectiondegree = document.getElementsByClassName("degree")
+  selectiondegree.style = "font: solid red"
+  selection = "radian"
 }
 
-function back()
-{
-    display.value=display.value.substr(0,display.value.length-1)
-    console.log(display.value)
-    }
+
+function insert(num){
+  var text = document.form.textinput.value  
+  document.form.textinput.value = text + num
+}
+
+function equal(){
+  var text = document.form.textinput.value
+  document.form.textinput.value = eval(text)
+}
+
+function back(){
+  var text = document.form.textinput.value
+  document.form.textinput.value = text.substring(0,text.length-1)
+}
+
+function remove(){
+  document.form.textinput.value = ' '
+}
+
+function sqr(){
+  document.form.textinput.value*2;
+}
+
+function loge(){
+  text = document.form.textinput.value
+  document.form.textinput.value = Math.log(text)
+}
+
+function rounde(){
+  text = document.form.textinput.value
+  document.form.textinput.value = Math.round(text)
+}
+
+function per(){
+  var text = document.form.textinput.value
+  document.form.textinput.value = text/100
+  }
+
+function sin(data){
+  var data1 = document.form.textinput.value;
+   
+  
+  // if (selection == "radian"){
+    // document.form.textinput.value = Math.sin(text)
+  // } else if (selection == "degree"){
+  //   document.form.textinput.value = Math.sin(text * (Math.PI / 180))
+  // }
+}
+
+function cos(){
+  console.log("cos")
+  var text = document.form.textinput.value
+  if (selection == "radian"){
+    document.form.textinput.value = Math.cos(text)
+  } else if (selection == "degree"){
+    document.form.textinput.value = Math.cos(text * (Math.PI / 180))
+  }
+}
+
+function tan(){
+  var text = document.form.textinput.value
+  if (selection == "radian"){
+    document.form.textinput.value = Math.sin(text)
+  } else if (selection == "degree"){
+    document.form.textinput.value = Math.sin(text * (Math.PI / 180))
+  }
+}
